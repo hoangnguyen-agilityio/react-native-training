a937b57bec0b9dd8b56b6998b73c08a779419610
Hoang Nguyen <hoang.nguyenviethuy@asnet.com.vn>
2022-04-26 16:31:22 +0700

    Update to run storybook

---
+++ componentsDocumentation/storybook/stories/FlatList/FlatList.stories.js
@@ -0,0 +1,16 @@
+import { storiesOf } from '@storybook/react-native';
+import React from 'react';
+import FlatList from '../../../components/FlatList';
+import Sectionlist from '../../../components/SectionList';
+import Virtualizedlist from '../../../components/VirtualizedList';
+import CenterView from '../CenterView';
+
+storiesOf('List', module)
+  .addDecorator((getStory) => <CenterView>{getStory()}</CenterView>)
+  .add('FlatList', () => (
+    <FlatList />
+  ))
+  .add('SectionList', () => (
+    <Sectionlist />
+  ))
+  .add('Virtualizedlist', () => <Virtualizedlist />)
