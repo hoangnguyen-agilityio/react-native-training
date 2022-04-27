f92403662c817115051133f2cfda10618052e129
Hoang Nguyen <hoang.nguyenviethuy@asnet.com.vn>
2022-04-27 12:22:36 +0700

    Init project

---
+++ HealthUI/storybook/storyLoader.js
@@ -0,0 +1,19 @@
+// Auto-generated file created by react-native-storybook-loader
+// Do not edit.
+//
+// https://github.com/elderfo/react-native-storybook-loader.git
+
+function loadStories() {
+  require('../src/components/stories/Button/Button.stories');
+  require('../src/components/stories/Welcome/Welcome.stories');
+}
+
+const stories = [
+  '../src/components/stories/Button/Button.stories',
+  '../src/components/stories/Welcome/Welcome.stories',
+];
+
+module.exports = {
+  loadStories,
+  stories,
+};
