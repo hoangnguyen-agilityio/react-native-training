dc4fd69e526b1f496d67682edbe85f57f6153887
HoangNguyen-AgilityIO <hoang.nguyen@iongroup.com>
2022-04-28 16:05:40 +0700

    Add custom fonts

--- HealthUI/index.js
+++ HealthUI/index.js
@@ -3,7 +3,10 @@
 // import App from './src/App';
 // import { name as appName } from './app.json';
 
+// import { Text } from 'react-native';
+
 // AppRegistry.registerComponent(appName, () => App);
+// Text.defaultProps.style = { fontFamily: 'SF Pro Display' };
 
 // Run storybook
 export { default } from './storybook';
