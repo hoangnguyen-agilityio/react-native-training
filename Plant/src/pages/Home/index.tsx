b68009e7f8e08c94b92052cc6bd929a26040c543
HoangNguyen-AgilityIO <hoang.nguyen@iongroup.com>
2022-05-17 16:54:47 +0700

    Implement header of HomePage

--- Plant/src/pages/Home/index.tsx
+++ Plant/src/pages/Home/index.tsx
@@ -1,8 +1,23 @@
 import React, { FC } from 'react';
-import { Text, View } from 'react-native';
+import { Image, Text, View } from 'react-native';
+import Header from '../../components/HeaderLayout';
+import COLORS from '../../constants/colors';
+import styles from './styles';
 
 const Home: FC = () => (
   <View>
+    <Header textBg="Home">
+      <View style={styles.headerContent}>
+        <View>
+          <Text style={styles.headerTitle}>Hello Taylor,</Text>
+          <Text style={styles.headerDesc}>Let’s Learn More About Plants</Text>
+        </View>
+        <Image
+          source={require('../../assets/images/avatar.png')}
+          style={styles.avatar}
+        />
+      </View>
+    </Header>
     <Text>Home page</Text>
   </View>
 );
--- Plant/src/pages/Home/styles.ts
+++ Plant/src/pages/Home/styles.ts
@@ -1,8 +1,33 @@
 import { StyleSheet } from 'react-native';
+import COLORS from '../../constants/colors';
 
 const styles = StyleSheet.create({
   root: {
     flex: 1,
+  },
+  headerContent: {
+    marginTop: 75,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    paddingHorizontal: 24,
+  },
+  headerTitle: {
+    fontFamily: 'SF-Pro-Display-Bold',
+    fontSize: 21,
+    lineHeight: 21,
+    marginBottom: 10,
+    color: COLORS.WHITE,
+  },
+  headerDesc: {
+    fontFamily: 'SF-Pro-Display-Regular',
+    fontSize: 14,
+    lineHeight: 14,
+    color: COLORS.WHITE,
+  },
+  avatar: {
+    width: 47,
+    height: 47,
   },
 });
 
